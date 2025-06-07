c047b4a852801d3cd9d836bb77728d148cc8ed96
Shaun Mattingly <shaun.d.mattingly@gmail.com>
2025-06-06 18:38:01 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const logo = document.querySelector('.logo a');
-    flickerEffect(logo, 5, 150);
     highlightActiveNav();
 });
-
-function flickerEffect(element, times, speed) {
-    let flickers = 0;
-    const interval = setInterval(() => {
-        element.style.opacity = (element.style.opacity === '0') ? '1' : '0';
-        flickers++;
-        if (flickers >= times * 2) {
-            element.style.opacity = '1';
-            clearInterval(interval);
-        }
-    }, speed);
-}
 
 function highlightActiveNav() {
     const path = window.location.pathname;
